6c0ce82fc2bcdcfba68a4cbd1fea0319e0c39473
Андрей Майоров <andreimaiorov@192.168.1.5>
2022-08-09 23:02:09 +0300

    Homework for lesson 9

--- src/main/scala/module2/homework_hkt_impllicts.scala
+++ src/main/scala/module2/homework_hkt_impllicts.scala
@@ -1,23 +1,43 @@
 package module2
 
-object homework_hkt_impllicts{
+object homework_hkt_impllicts extends App {
 
-    /**
-      * 
-      * Доработать сигнатуру tupleF и реализовать его
-      * По итогу должны быть возможны подобные вызовы
-      *   val r1 = println(tupleF(optA, optB))
-      *   val r2 = println(tupleF(list1, list2))
-      * 
-      */
-    def tupleF[F[_], A, B](fa: F[A], fb: F[B]) = ???
+  /**
+   *
+   * Доработать сигнатуру tupleF и реализовать его
+   * По итогу должны быть возможны подобные вызовы
+   *   val r1 = println(tupleF(optA, optB))
+   *   val r2 = println(tupleF(list1, list2))
+   *
+   */
 
+  def tupleF[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] = {
+    tupleBindable(fa, fb)
+  }
 
-    trait Bindable[F[_], A] {
-        def map[B](f: A => B): F[B]
-        def flatMap[B](f: A => F[B]): F[B]
-    }
+  trait Bindable[F[_], A] {
+    def map[B](f: A => B): F[B]
+    def flatMap[B](f: A => F[B]): F[B]
+  }
 
+  def tupleBindable[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
+    fa.flatMap{ a => fb.map((a, _))}
+
+  def optBindable[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+    override def map[B](f: A => B): Option[B] = opt.map(f)
+
+    override def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
+  }
+
+  def listBindable[A](list: List[A]): Bindable[List, A] = new Bindable[List, A] {
+    override def map[B](f: A => B): List[B] = list.map(f)
+
+    override def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
+  }
+
+  implicit def optionToBindable[A](option: Option[A]): Bindable[Option, A] = optBindable(option)
+
+  implicit def listToBindable[A](list: List[A]): Bindable[List, A] = listBindable(list)
 
   val optA: Option[Int] = Some(1)
   val optB: Option[Int] = Some(2)
@@ -25,6 +45,6 @@ object homework_hkt_impllicts{
   val list1 = List(1, 2, 3)
   val list2 = List(4, 5, 6)
 
-  val r1 = println(tupleF(optA, optB))
-  val r2 = println(tupleF(list1, list2))
+  println(tupleF(optA, optB))
+  println(tupleF(list1, list2))
 }
